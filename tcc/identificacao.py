3f980ba8e2c68ebabe7077478dde17972b957130
Matheus <mathtass@hotmail.com>
2021-05-07 23:56:14 -0300

    shape

--- tcc/identificacao.py
+++ tcc/identificacao.py
@@ -3,25 +3,26 @@ import numpy as np
 import pdb
 
 
-img = cv2.imread('escorpiao.png')
+img = cv2.imread('esc.png')
 
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 ## mask of green (36,25,25) ~ (86, 255,255)
 #Red color rangle  169, 100, 100 , 189, 255, 255
 
-lower_range = np.array([50,252,252])
-upper_range = np.array([185, 255,252])
+# lower_range = np.array([50,252,252])
+# upper_range = np.array([175, 255,252])
 
-# lower_range = np.array([40, 40,40])
-# upper_range = np.array([70,255,255])
+lower_range = np.array([45,222,252])
+upper_range = np.array([155, 255,252])
+
 
 mask = cv2.inRange(hsv, lower_range, upper_range)
 
 if cv2.countNonZero(mask) > 0:
-    print('Escorpion is present!')
+    print('Escorpiao presente')
 else:
-    print('Escorpion is not present!')
+    print('Escorpiao nao encontrado')
 
 cv2.imshow('image', img)
 cv2.imshow('mask', mask)
